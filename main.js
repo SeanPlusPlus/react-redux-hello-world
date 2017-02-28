a96ba30e9b45b15a63efd5e86f31bb1707674df6
Sean Stephenson <sms@pixar.com>
2017-02-28 12:01:08 -0800

    refactor reducers

--- main.js
+++ main.js
@@ -1,37 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers,
-         applyMiddleware, bindActionCreators } from 'redux';
+import { createStore, applyMiddleware, bindActionCreators } from 'redux';
 import Hello from './hello';
 import World from './world';
-
-const greetingReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SAY_HELLO':
-      return 'Hello ';
-    case 'SAY_GOODBYE':
-      return 'Bye '
-  }
-  return state;
-}
-
-const nameReducer = (state = 'Dude', action) => {
-  switch (action.type) {
-    case 'CHANGE_NAME':
-      return 'Yo';
-  }
-  return state;
-}
-
-const actionLogger = ({dispatch, getState}) => (next) => (action) => {
-  console.log(action);
-  return next(action)
-};
-
-const reducers = combineReducers({
-  greeting: greetingReducer,
-  name: nameReducer,
-});
+import { reducers, actionLogger } from './reducers';
 
 const middleware = applyMiddleware(actionLogger);
 const store = createStore(
