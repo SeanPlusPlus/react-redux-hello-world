73eb1c436159371af1b5d60feb5bae3e46f7499f
Sean Stephenson <sms@pixar.com>
2017-02-28 18:13:29 -0800

    refactor class for Navigation

--- navigation.js
+++ navigation.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 
-class Navigation extends React.Component {
-  render() {
-    return (
-      <div>
-        <Nav className="navbar navbar-light bg-faded rounded navbar-toggleable-md fixed-top">
-          <NavItem>
-            <NavLink href="#">Home</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">About</NavLink>
-          </NavItem>
-        </Nav>
-      </div>
-    )
-  }
-}
+const Navigation = () => (
+  <div>
+    <Nav className="navbar navbar-light bg-faded rounded navbar-toggleable-md fixed-top">
+      <NavItem>
+        <NavLink href="#">Home</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">About</NavLink>
+      </NavItem>
+    </Nav>
+  </div>
+)
 
 export default Navigation;
